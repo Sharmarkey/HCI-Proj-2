0a4ce08214d4e4fc06c913f17c9cd2ba102da644
CElbing <coleelbing1@gmail.com>
2024-12-03 17:21:29 -0600

    added header and footer content, altered account page header

--- PHP-Login-System/account.php
+++ PHP-Login-System/account.php
@@ -31,39 +31,145 @@ if (isset($_GET['logout'])) {
 </head>
 
 <body>
-    <div>
-        <header>
-            <h2 class="header-regular">Welcome <?php echo $_SESSION['user']; ?>!</h2>
-            <a href="?logout">Logout</a>
-        </header>
-        <main>
-            <div class = "protected-page-content-container">
-                <article class=protected-page-content>
-                    <h1 class="header-regular">Policies</h1>
-                    <p class=paragraph-regular>Here is the Bridges Health list of policies
-                        that are to be followed by all staff and volunteers.
-                        Please refer to this list as needed.</p>
-                </article>
-                <article class=protected-page-content>
-                    <h1 class="header-regular">Procedures</h1>
-                    <p class=paragraph-regular>Here are the Bridges Health step-by-step procedures
-                        that are to be followed by all staff and volunteers.
-                        Please refer to this list as needed.</p>
-                </article>
-                <article class=protected-page-content>
-                    <h1 class="header-regular">Training Videos</h1>
-                    <p class=paragraph-regular>Here are the Bridges Health training videos that will give you
-                        visual demonstrations of different tasks you will be expected to carry out during your time with us.
-                        Please feel free to review these videos as needed.</p>
-                </article>
-            </div>
-        </main>
+    <header class="header-flex-container" id="account-page-header">
+        <div class="appointment-social-media-header" id="only-desktop">
+            <h1 class="header-regular appointment-social-media-header-content">(507) 474-2221</h1>
+            <p class="header-regular-small appointment-social-media-header-content">Call for Appointment</p>
+            <ul class="social-media-list-flex-container appointment-social-media-header-content">
+                <li class="nav-list-item paragraph-regular">
+                    <a href="https://www.facebook.com/profile.php?id=61562904845913" class="social-media-list-icon">
+                        <img class="social-media-icon" src="/assets/icons/fb_primary.ico" />
+                    </a>
+                </li>
+                <li class="nav-list-item paragraph-regular">
+                    <a href="https://www.youtube.com/channel/UCkqAJX7E3-nzu064ilpDDvw" class="social-media-list-icon">
+                        <img class="social-media-icon" src="/assets/icons/yt_primary.ico" />
+                    </a>
+                </li>
+                <li class="nav-list-item paragraph-regular">
+                    <a href="https://www.instagram.com/bridgescommunityhealth_/" class="social-media-list-icon">
+                        <img class="social-media-icon" src="/assets/icons/ig_primary.ico" />
+                    </a>
+                </li>
+                <li class="nav-list-item paragraph-regular">
+                    <a href="https://www.linkedin.com/company/bridges-health-winona?trk=public_profile_topcard-current-company"
+                        class="social-media-list-icon">
+                        <img class="social-media-icon" src="/assets/icons/in_primary.ico" />
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <a href="/donation.html">
+            <img class="primary-logo" src="https://bridgeshealthwinona.org/wp-content/uploads/2024/08/logo-main.png">
+        </a>
+        <div class="header-button-container">
+            <h2 class="header-regular" id = "account-info">Welcome <?php echo $_SESSION['user']; ?>!</h2>
+            <button class="donation-button-header" id="logout-button" href="?logout">Logout</button>
+        </div>
+    </header>
+    <div class="nav-bar">
+        <ul class="nav-list-flex-container">
+            <li class="nav-list-item paragraph-regular">
+                <a class="darken-nav-button" href="donation.html">
+                    <button class="nav-button"> Donate </button>
+                </a>
+            </li>
+            <li class="nav-list-item paragraph-regular">
+                <a href="PHP-Login-System/login.php">
+                    <button class="nav-button"> Login </button>
+                </a>
+            </li>
+            <li class="nav-list-item paragraph-regular">
+                <a href="instagram_feed.html">
+                    <button class="nav-button"> Social Media </button>
+                </a>
+            </li>
+        </ul>
     </div>
-    <footer>
+    <div class="protected-page-content-container">
+        <article class=protected-page-content>
+            <h1 class="header-regular">Policies</h1>
+            <p class=paragraph-regular>Here is the Bridges Health list of policies
+                that are to be followed by all staff and volunteers.
+                Please refer to this list as needed.</p>
+        </article>
+        <article class=protected-page-content>
+            <h1 class="header-regular">Procedures</h1>
+            <p class=paragraph-regular>Here are the Bridges Health step-by-step procedures
+                that are to be followed by all staff and volunteers.
+                Please refer to this list as needed.</p>
+        </article>
+        <article class=protected-page-content>
+            <h1 class="header-regular">Training Videos</h1>
+            <p class=paragraph-regular>Here are the Bridges Health training videos that will give you
+                visual demonstrations of different tasks you will be expected to carry out during your time with us.
+                Please feel free to review these videos as needed.</p>
+        </article>
+    </div>
+    <div>
         <form action="delete_account.php" method="POST" onsubmit="return confirm('Are you sure you want to delete your account?');">
             <input type="hidden" name="username" value="<?php echo htmlspecialchars($_SESSION['user']); ?>">
             <button type="submit" name="delete_account">Delete Account</button>
         </form>
+    </div>
+    <footer class="footer">
+        <div class="footer-content">
+            <div class="quick-link-container">
+                <h1 class="header-regular appointment-social-media-header-content">Contact</h1>
+                <p class="header-regular-small appointment-social-media-header-content">(507) 474-2221</p>
+            </div>
+            <div class="quick-link-container">
+                <img class="favicon-footer" src="https://bridgeshealthwinona.org/wp-content/uploads/2024/08/heart.png">
+                <ul class="social-media-list-flex-container appointment-social-media-header-content">
+                    <li class="nav-list-item paragraph-regular">
+                        <a href="https://www.facebook.com/profile.php?id=61562904845913" class="social-media-list-icon">
+                            <img class="social-media-icon" src="/assets/icons/fb_white.ico" />
+                        </a>
+                    </li>
+                    <li class="nav-list-item paragraph-regular">
+                        <a href="https://www.youtube.com/channel/UCkqAJX7E3-nzu064ilpDDvw"
+                            class="social-media-list-icon">
+                            <img class="social-media-icon" src="/assets/icons/yt_white.ico" />
+                        </a>
+                    </li>
+                    <li class="nav-list-item paragraph-regular">
+                        <a href="https://www.instagram.com/bridgescommunityhealth_/" class="social-media-list-icon">
+                            <img class="social-media-icon" src="/assets/icons/ig_white.ico" />
+                        </a>
+                    </li>
+                    <li class="nav-list-item paragraph-regular">
+                        <a href="https://www.linkedin.com/company/bridges-health-winona?trk=public_profile_topcard-current-company"
+                            class="social-media-list-icon">
+                            <img class="social-media-icon" src="/assets/icons/in_white.ico">
+                        </a>
+                    </li>
+                </ul>
+                <p class="paragraph-regular copy-write">© 2024 Bridges Health. A non-profit 501(c)(3).
+                    <br>
+                    "All Rights Reserved."
+                </p>
+            </div>
+            <div class="quick-link-container">
+                <p class="header-bold-small">Quick Links</p>
+                <ul class="quick-link-list-container">
+                    <li class="quick-link-item">
+                        <a class="quick-link" href="donation.html">
+                            <p class="paragraph-regular ">Donate</p>
+                        </a>
+                    </li>
+                    <li class="quick-link-item">
+                        <a class="quick-link" href="instagram_feed.html">
+                            <p class="paragraph-regular">Social Media</p>
+                        </a>
+                    </li>
+                    <li class="quick-link-item">
+                        <a class="quick-link" href="PHP-Login-System/login.php">
+                            <p class="paragraph-regular">Login</p>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </footer>
 </body>
 
--- PHP-Login-System/login.php
+++ PHP-Login-System/login.php
@@ -22,7 +22,66 @@
         href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap">
     <link rel="stylesheet" href="/css/styles.css">
 </head>
-<body class = "center-form">
+<body>
+<header class="header-flex-container">
+        <div class="appointment-social-media-header" id="only-desktop">
+            <h1 class="header-regular appointment-social-media-header-content">(507) 474-2221</h1>
+            <p class="header-regular-small appointment-social-media-header-content">Call for Appointment</p>
+            <ul class="social-media-list-flex-container appointment-social-media-header-content">
+                <li class="nav-list-item paragraph-regular">
+                    <a href="https://www.facebook.com/profile.php?id=61562904845913" class="social-media-list-icon">
+                        <img class="social-media-icon" src="/assets/icons/fb_primary.ico" />
+                    </a>
+                </li>
+                <li class="nav-list-item paragraph-regular">
+                    <a href="https://www.youtube.com/channel/UCkqAJX7E3-nzu064ilpDDvw" class="social-media-list-icon">
+                        <img class="social-media-icon" src="/assets/icons/yt_primary.ico" />
+                    </a>
+                </li>
+                <li class="nav-list-item paragraph-regular">
+                    <a href="https://www.instagram.com/bridgescommunityhealth_/" class="social-media-list-icon">
+                        <img class="social-media-icon" src="/assets/icons/ig_primary.ico" />
+                    </a>
+                </li>
+                <li class="nav-list-item paragraph-regular">
+                    <a href="https://www.linkedin.com/company/bridges-health-winona?trk=public_profile_topcard-current-company"
+                        class="social-media-list-icon">
+                        <img class="social-media-icon" src="/assets/icons/in_primary.ico" />
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <a href="/donation.html">
+            <img class="primary-logo" src="https://bridgeshealthwinona.org/wp-content/uploads/2024/08/logo-main.png">
+        </a>
+        <div class="header-button-container" id="only-desktop">
+            <a href="dontation-thank-you-page.html">
+                <button class="volunteer-button-header" id="volunteer-button">Volunteer</button>
+            </a>
+            <a href="dontation-thank-you-page.html">
+                <button class="donation-button-header" id="donation-button">Donate</button>
+            </a>
+        </div>
+    </header>
+    <div class="nav-bar">
+        <ul class="nav-list-flex-container">
+            <li class="nav-list-item paragraph-regular">
+                <a class="darken-nav-button" href="donation.html">
+                    <button class="nav-button"> Donate </button>
+                </a>
+            </li>
+            <li class="nav-list-item paragraph-regular">
+                <a href="PHP-Login-System/login.php">
+                    <button class="nav-button"> Login </button>
+                </a>
+            </li>
+            <li class="nav-list-item paragraph-regular">
+                <a href="instagram_feed.html">
+                    <button class="nav-button"> Social Media </button>
+                </a>
+            </li>
+        </ul>
+    </div>
     <div class = form-container>
         <div class="form-content">
             <form action="" method="post" enctype="multipart/form-data" autocomplete="off">
@@ -46,5 +105,64 @@
             </form>
         </div>
     </div>
+    <footer class="footer">
+        <div class="footer-content">
+            <div class = "quick-link-container">
+                <h1 class="header-regular appointment-social-media-header-content">Contact</h1>
+                <p class="header-regular-small appointment-social-media-header-content">(507) 474-2221</p>
+            </div>
+            <div class = "quick-link-container">
+                <img class="favicon-footer" src="https://bridgeshealthwinona.org/wp-content/uploads/2024/08/heart.png">
+                <ul class="social-media-list-flex-container appointment-social-media-header-content">
+                    <li class="nav-list-item paragraph-regular">
+                        <a href="https://www.facebook.com/profile.php?id=61562904845913" class="social-media-list-icon">
+                            <img class="social-media-icon" src="/assets/icons/fb_white.ico" />
+                        </a>
+                    </li>
+                    <li class="nav-list-item paragraph-regular">
+                        <a href="https://www.youtube.com/channel/UCkqAJX7E3-nzu064ilpDDvw"
+                            class="social-media-list-icon">
+                            <img class="social-media-icon" src="/assets/icons/yt_white.ico" />
+                        </a>
+                    </li>
+                    <li class="nav-list-item paragraph-regular">
+                        <a href="https://www.instagram.com/bridgescommunityhealth_/" class="social-media-list-icon">
+                            <img class="social-media-icon" src="/assets/icons/ig_white.ico" />
+                        </a>
+                    </li>
+                    <li class="nav-list-item paragraph-regular">
+                        <a href="https://www.linkedin.com/company/bridges-health-winona?trk=public_profile_topcard-current-company"
+                            class="social-media-list-icon">
+                            <img class="social-media-icon" src="/assets/icons/in_white.ico">
+                        </a>
+                    </li>
+                </ul>
+                <p class="paragraph-regular copy-write">© 2024 Bridges Health. A non-profit 501(c)(3).
+                    <br>
+                    "All Rights Reserved."
+                </p>
+            </div>
+            <div class = "quick-link-container">
+                <p class = "header-bold-small">Quick Links</p>
+                <ul class = "quick-link-list-container">
+                    <li class="quick-link-item">
+                        <a class="quick-link" href="donation.html">
+                            <p class="paragraph-regular ">Donate</p>
+                        </a>
+                    </li>
+                    <li class="quick-link-item">
+                        <a class="quick-link" href="instagram_feed.html">
+                            <p class="paragraph-regular">Social Media</p>
+                        </a>
+                    </li>
+                    <li class="quick-link-item">
+                        <a class="quick-link" href="PHP-Login-System/login.php">
+                            <p class="paragraph-regular">Login</p>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </footer>
 </body>
 </html>
